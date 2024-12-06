11d7fabbb3e29ff75bbba3463eaa81a23e72eacb
PedroLucas <118374542+ETHGWRTJTYJ@users.noreply.github.com>
2024-12-06 11:12:53 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+
 app.get("/produtos", async(req,res)=>{
-    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
-    //1 - Criar a conexão com o banco
     try{
         const conection = await mysql.createConnection({
             host:process.env.dbhost?process.env.dbhost:"localhost",
@@ -15,14 +15,77 @@ app.get("/produtos", async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"banco1022a",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        //2 - Realizar uma consulta na tabela
         const [result, fields] = await conection.query("SELECT * from produtos")
-        //3 - Devolver os dados pra quem pediu
         res.send(result)
     }catch(e){
         res.status(500).send("Server ERROR")
     }
 })
+
+app.post("/produtos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const {id,nome,tamanho,preco,imagem,marca,modelo} = req.body
+        const [result, fields] = await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?,?,?)",
+            [id,nome,tamanho,preco,imagem,marca,modelo])
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
+
+
+
+
+
+
+
+
+
+app.get("/usuarios", async(req,res)=>{
+    try{
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const [result, fields] = await conection.query("SELECT * from produtos")
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Server ERROR")
+    }
+})
+
+app.post("/produtos", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022a",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        })
+        const {id,nome,cpf,senha,confirmarsenha,email,numero,endereco,idade} = req.body
+        const [result, fields] = await connection.query("INSERT INTO produtos VALUES (?,?,?,?,?,?,?,?,?)",
+            [id,nome,cpf,senha,confirmarsenha,email,numero,endereco,idade])
+        await connection.end()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
+
 app.listen(8000,()=>{
-    console.log("Iniciei o servidor")
+    console.log("servidor Iniciado")
 })
